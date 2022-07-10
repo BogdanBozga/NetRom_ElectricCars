294f7f3e780115b520c0fe8a33301ff0a7ea815b
BogdanBozga <bogdan.bozga01@gmail.com>
2022-07-10 03:23:35 -0700

    Adding booking menu and some datetime verification

---
+++ ElectricCars_NetRom/Controllers/BookingController.cs
@@ -0,0 +1,63 @@
+﻿using ElectricCars_NetRom.Models.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ElectricCars_NetRom.Models.ViewModel;
+
+namespace ElectricCars_NetRom.Controllers
+{
+    public class BookingController : Controller
+    {
+
+
+        private ElectricCars_NetRomContext _Context;
+
+        public BookingController(ElectricCars_NetRomContext dbConn)
+        {
+            _Context = dbConn;
+        }
+
+
+        public IActionResult Index(int Id)
+        {
+            Booking booking = new Booking();
+            booking.PlugId = Id;
+            return View(booking);
+        }
+
+
+        [HttpPost]
+        [ActionName("SaveBooking")]
+        public IActionResult SaveBoooking(Booking booking)
+        {
+                ModelState.Clear();
+                if (booking.StartDate > booking.EndDate)
+                {
+                    ModelState.AddModelError(nameof(Booking.StartDate), "Start date can't be after end date");
+                } 
+                if( booking.StartDate < DateTime.Now+new TimeSpan(0,1,0))
+                {
+                    ModelState.AddModelError(nameof(Booking.StartDate), "Start date can't be in the past");
+                }
+                if(booking.StartDate + new TimeSpan(0,29,0) > booking.EndDate)
+                {
+                    ModelState.AddModelError(nameof(Booking.EndDate), "Minimal recharging time is 30 minutes");
+                }
+                if(ModelState.IsValid){
+                    Booking newBookingSave = new Booking();
+                    newBookingSave.StartDate = booking.StartDate;
+                    newBookingSave.EndDate = booking.EndDate;
+                    newBookingSave.CarNumber = booking.CarNumber;
+                    newBookingSave.PlugId = booking.PlugId;
+                    newBookingSave.Plug = _Context.Plugs.First(x => x.Id == booking.PlugId);
+                    _Context.Add(newBookingSave);
+                    _Context.SaveChanges();
+                    return RedirectToAction("Details", "RechargingStation",new {id = _Context.Plugs.First(m => m.Id == booking.PlugId).StationId});
+                }
+                else
+                {
+                    return View("Index", booking);
+                }
+
+        }
+    }
+}
--- ElectricCars_NetRom/Models/DB/Booking.cs
+++ ElectricCars_NetRom/Models/DB/Booking.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ElectricCars_NetRom.Models.DB
 {
     public partial class Booking
     {
         public Guid Id { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; } = DateTime.Now + new TimeSpan(0,30,0);
+
+
+        [Display(Name = "Car number")]
         public string CarNumber { get; set; } = null!;
         public int PlugId { get; set; }
         public Guid? UserId { get; set; }
